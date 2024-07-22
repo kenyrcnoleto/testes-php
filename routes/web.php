fa69c412f17989d8550da7ab534761fc4c14bee7
Keny R C Noleto <kenyrcnoleto@gmail.com>
2024-07-22 12:19:04 -0300

    testando page-response-status

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/404', function() {
+    return ['oi'];
+});
+
+Route::get('/403', function() {
+
+    abort_if(true,403);
+
+    return ['oi'];
+});
