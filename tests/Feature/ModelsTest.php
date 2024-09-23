dfa7f0ee4e2f6539f6bbebb430ae9459eea568c4
Keny R C Noleto <kenyrcnoleto@gmail.com>
2024-09-23 12:20:57 -0300

    model relationship

---
+++ tests/Feature/ModelsTest.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Models\Product;
+use App\Models\User;
+
+test('model relationship :: product owner should be an user', function () {
+
+    $user = User::factory()->create();
+
+    $product = Product::factory()->create(['owner_id' => $user->id]);
+
+    $owner = $product->owner;
+
+    expect($owner)
+        ->toBeInstanceOf(User::class);
+});
+
