ec64570cbf836be7680fbe16bc98531fe024c9f2
Keny R C Noleto <kenyrcnoleto@gmail.com>
2024-10-03 17:23:41 -0300

    actions :: making sure that the action do what supposed to do

--- tests/Feature/ActionsTest.php
+++ tests/Feature/ActionsTest.php
@@ -2,17 +2,20 @@
 
 use App\Actions\CreateProductAction;
 use App\Models\User;
-use Illuminate\Notifications\Notification;
-use Illuminate\Support\Facades\Notification as FacadesNotification;
+use App\Notifications\NewProductionNotification;
+//use Illuminate\Notifications\Notification;
+use Illuminate\Support\Facades\Notification;
 
 use function Pest\Laravel\actingAs;
+use function Pest\Laravel\assertDatabaseCount;
+use function Pest\Laravel\assertDatabaseHas;
 use function Pest\Laravel\postJson;
 
-test('it should the action to create a product', function () {
+it('it should the call the action to create a product', function () {
     //Actions não é do próprio Laravel, por isso deve trabalhar de forma diferente
 
     //Queue::fake();
-    FacadesNotification::fake();
+    Notification::fake();
 
     //Neste caso muda um pouco em rels as posições do assert, arrange, act
     //Assert - Verifica se esse cara será chamado
@@ -31,4 +34,22 @@ test('it should the action to create a product', function () {
     //Act - quando eu agir desta maneira
 
     postJson(route('product.store'), ['title' => $title]);
+});
+
+it('should be able to create a product', function () {
+
+    Notification::fake();
+
+    $user = User::factory()->create();
+
+    (new CreateProductAction())->handle('Product 1', $user);
+
+    assertDatabaseCount('products', 1);
+
+    assertDatabaseHas('products', [
+        'title' => 'Product 1',
+        'owner_id' => $user->id
+    ]);
+
+    Notification::assertSentTo([$user], NewProductionNotification::class);
 });
